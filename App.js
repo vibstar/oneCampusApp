fe3fce7ff3cda7609cf040a65dff5f1e3aef66e6
Vibhu Singh <vibhusingh@brandeis.edu>
2021-11-12 03:22:49 -0500

    Updated directories, added pictures, and set up the basics to continue developing in the morning. Added about page, and food menu.

--- App.js
+++ App.js
@@ -1,57 +1,223 @@
-
 import * as React from 'react';
-import { Button, StyleSheet, View, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Button, FlatList, View, Text, Image, Pressable, SectionList, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const MENU = [
+  {
+    id: '1',
+    title: "Macaroni and Cheese"
+  },
+  {
+    id: '2',
+    title: "Pizza"
+  },
+  {
+    id: '3',
+    title: "Noodles"
+  },
+  {
+    id: '4',
+    title: "French Fries"
+  },
+  {
+    id: '5',
+    title: "Falafel"
+  },
+  {
+    id: '6',
+    title: "Baked Ziti"
+  },
+  {
+    id: '7',
+    title: "Tonkatsu Ramen"
+  },
+  {
+    id: '8',
+    title: "Tom Yum Noodle Soup"
+  },
+  {
+    id: '9',
+    title: "Burrito"
+  },
+  {
+    id: '10',
+    title: "3 Tacos"
+  },
+  {
+    id: '11',
+    title: "Thai Basil Chow Fun"
+  },
+  {
+    id: '12',
+    title: "Eggplant Parmesean Sub"
+  }
+]
+
+const HOMELIST = [
+  {
+    title: 'You can order all sorts of food from the One Campus App',
+    data: ["Check out the menu"],
+    index: 'Menu'
+  },
+  {
+    title: 'You can check your notifications',
+    data: ["Go to notifications"],
+    index: 'Notifications'
+  }
+]
+
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
+
+    <View style={(styles.homePage)}>
+      <Image style={(styles.brandeisLogo)} source={require("./Brandeis_University_seal.svg.png")} />
+      <ScrollView>
+      <Text style={(styles.regularFont)}>Here is a short about page about the app creator</Text>
+      <View style={(styles.navigationButtonStyle)}>
+        <Pressable style={styles.navigationButtonStyle} onPress={() => navigation.navigate('About')}>
+          <Text>Go to about page</Text>
+        </Pressable>
+      </View>
+      <Text style={(styles.regularFont)}>You can order all sorts of food from the One Campus App</Text>
+      <View style={(styles.navigationButtonStyle)}>
+        <Pressable style={styles.navigationButtonStyle} onPress={() => navigation.navigate('Menu')}>
+          <Text>Go to menu</Text>
+        </Pressable>
+        </View>
+      </ScrollView>
+      
+
+
+      {/* <SectionList
+      sections={HOMELIST}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({ item }) => 
+        <Item title={item} />}
+      renderSectionHeader={({ section: { title } }) => (
+        <Text style={styles.bigFont}>{title}</Text>
+      )}
+    /> */}
     </View>
   );
 }
 
-function DetailsScreen({ navigation }) {
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.regularFont}>{title}</Text>
+  </View>
+);
+
+const renderItem = ({ item }) => (
+  <Item title={item.title} />
+);
+
+function MenuScreen({ navigation }) {
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={MENU}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
+  );
+}
+
+function AboutScreen({ navigation }) {
+  return (
+
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', marginTop: '5%' }}>
+      <ScrollView style={styles.scrollView}>
+        <Image style={(styles.vibhuPic)} source={require("./VibhuPicture.jpg")} />
+        <Text style={styles.aboutPageText}>
+          {'\t'}This app is made by Vibhu Singh. He is a junior at Brandeis University studying 
+          computer science and economics. He is also working towards his minor in philosophy. 
+          He is 20 years old and is trying to figure out what he wants to do with his life.
+
+        </Text>
+        <Text style={styles.aboutPageText}>
+          {'\t'}The goal of this app is to consolidate all the Brandeis related apps into 
+          one app. This includes Bite, Get, the campus passport and so on.
+        </Text>
+      </ScrollView>
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button
-        title="Go back to first screen in stack"
-        onPress={() => navigation.popToTop()}
-      />
+
     </View>
   );
 }
 
 const Stack = createNativeStackNavigator();
 
-function App() {
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="One Campus At Brandeis" component={HomeScreen} />
+      <Stack.Screen name="About" component={AboutScreen} />
+      <Stack.Screen name="Menu" component={MenuScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+      <MyStack />
     </NavigationContainer>
   );
 }
+
 const styles = StyleSheet.create({
-  container: {
+  homePage: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start'
   },
-});
-export default App;
+  navigationButtonStyle: {
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    padding: 2,
+    marginVertical: 8,
+    marginHorizontal: '20%',
+    fontSize: 14,
+  },
+  brandeisLogo: {
+    width: '100%',
+    height: undefined,
+    aspectRatio: 1,
+  },
+  item: {
+    backgroundColor: 'orange',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  regularFont: {
+    fontSize: 18,
+  },
+  homepageButtons: {
+    fontSize: 12
+  },
+  scrollView: {
+    marginHorizontal: '5%',
+  },
+  aboutPageText: {
+    fontSize: 24
+  },
+  vibhuPic: {
+    alignItems: 'center',
+    width: '95%',
+    height: undefined,
+    aspectRatio: 9 / 16
+  }
+})
+
