4eb2c5e8e0022a2183d7986c8fe68f89f3d15d0c
Vibhu Singh <vibhusingh@brandeis.edu>
2021-11-12 10:13:21 -0500

    Added covid passport login page and the actual passport page

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import { SafeAreaView, StyleSheet, Button, FlatList, View, Text, Image, Pressable, SectionList, ScrollView } from 'react-native';
+import { SafeAreaView, StyleSheet, Button, FlatList, View, Text, Image, Pressable, TextInput, ScrollView, Alert } from 'react-native';
+import { useState, useEffect } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const MENU = [
   {
@@ -54,50 +56,31 @@ const MENU = [
   }
 ]
 
-const HOMELIST = [
-  {
-    title: 'You can order all sorts of food from the One Campus App',
-    data: ["Check out the menu"],
-    index: 'Menu'
-  },
-  {
-    title: 'You can check your notifications',
-    data: ["Go to notifications"],
-    index: 'Notifications'
-  }
-]
-
 function HomeScreen({ navigation }) {
   return (
 
     <View style={(styles.homePage)}>
       <Image style={(styles.brandeisLogo)} source={require("./Brandeis_University_seal.svg.png")} />
       <ScrollView>
-      <Text style={(styles.regularFont)}>Here is a short about page about the app creator</Text>
-      <View style={(styles.navigationButtonStyle)}>
-        <Pressable style={styles.navigationButtonStyle} onPress={() => navigation.navigate('About')}>
-          <Text>Go to about page</Text>
-        </Pressable>
-      </View>
-      <Text style={(styles.regularFont)}>You can order all sorts of food from the One Campus App</Text>
-      <View style={(styles.navigationButtonStyle)}>
-        <Pressable style={styles.navigationButtonStyle} onPress={() => navigation.navigate('Menu')}>
-          <Text>Go to menu</Text>
-        </Pressable>
+        <Text style={(styles.regularFont)}>Here is a short about page about the app creator</Text>
+        <View style={(styles.navigationButtonStyle)}>
+          <Pressable style={styles.navigationButtonStyle} onPress={() => navigation.navigate('About')}>
+            <Text>Go to about page</Text>
+          </Pressable>
+        </View>
+        <Text style={(styles.regularFont)}>You can order all sorts of food from the One Campus App</Text>
+        <View style={(styles.navigationButtonStyle)}>
+          <Pressable style={styles.navigationButtonStyle} onPress={() => navigation.navigate('Menu')}>
+            <Text>Go to menu</Text>
+          </Pressable>
+        </View>
+        <Text style={(styles.regularFont)}>You can access your COVID-19 campus passport from here as well</Text>
+        <View style={(styles.navigationButtonStyle)}>
+          <Pressable style={styles.navigationButtonStyle} onPress={() => navigation.navigate('Covid Login')}>
+            <Text>Go to COVID-19 Campus Passport</Text>
+          </Pressable>
         </View>
       </ScrollView>
-      
-
-
-      {/* <SectionList
-      sections={HOMELIST}
-      keyExtractor={(item, index) => item + index}
-      renderItem={({ item }) => 
-        <Item title={item} />}
-      renderSectionHeader={({ section: { title } }) => (
-        <Text style={styles.bigFont}>{title}</Text>
-      )}
-    /> */}
     </View>
   );
 }
@@ -127,18 +110,17 @@ function MenuScreen({ navigation }) {
 
 function AboutScreen({ navigation }) {
   return (
-
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', marginTop: '5%' }}>
       <ScrollView style={styles.scrollView}>
         <Image style={(styles.vibhuPic)} source={require("./VibhuPicture.jpg")} />
-        <Text style={styles.aboutPageText}>
-          {'\t'}This app is made by Vibhu Singh. He is a junior at Brandeis University studying 
-          computer science and economics. He is also working towards his minor in philosophy. 
+        <Text style={styles.mediumFont}>
+          {'\t'}This app is made by Vibhu Singh. He is a junior at Brandeis University studying
+          computer science and economics. He is also working towards his minor in philosophy.
           He is 20 years old and is trying to figure out what he wants to do with his life.
 
         </Text>
-        <Text style={styles.aboutPageText}>
-          {'\t'}The goal of this app is to consolidate all the Brandeis related apps into 
+        <Text style={styles.mediumFont}>
+          {'\t'}The goal of this app is to consolidate all the Brandeis related apps into
           one app. This includes Bite, Get, the campus passport and so on.
         </Text>
       </ScrollView>
@@ -146,11 +128,87 @@ function AboutScreen({ navigation }) {
   );
 }
 
-function SettingsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Go back" onPress={() => navigation.goBack()} />
 
+
+function CovidLoginScreen({ navigation }) {
+  const [userID, setUserID] = useState('');
+  const [password, setPassword] = useState('');
+  const login = async () => {
+    if (userID.length == 0) {
+      Alert.alert('Warning!', 'You have to put in your username')
+    } else if (password.length == 0) {
+      Alert.alert('Warning!', 'You have to put in your password')
+    } else {
+      try {
+        await AsyncStorage.setItem('Username', userID);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    if (userID.length > 0 && password.length > 0) {
+      navigation.navigate('Covid Passport')
+    }
+
+  }
+  return (
+    <View style={{ flex: 1, flexDirection: 'vertical', alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flexDirection: 'row' }}>
+        <Text style={styles.mediumFont}>Username:</Text>
+        <TextInput
+          style={styles.loginBox}
+          onChangeText={(val) => setUserID(val)}
+        />
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <Text style={styles.mediumFont}>Password:</Text>
+        <TextInput
+          style={styles.loginBox}
+          onChangeText={(val) => setPassword(val)}
+        />
+      </View>
+      <View style={(styles.navigationButtonStyle)}>
+        <Pressable style={styles.navigationButtonStyle} onPress={login}>
+          <Text>login</Text>
+        </Pressable>
+      </View>
+      
+
+    </View>
+  );
+}
+
+function CovidPassportScreen({ navigation }) {
+  const [username, setName] = useState('');
+  const [date, setDate] = useState('')
+  const getUsername = () => {
+    try {
+      AsyncStorage.getItem('Username')
+        .then(value => {
+          if (value != null) {
+            setName(value);
+          }
+        })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getUsername();
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+    setDate(month + '/' + date + '/' + year)
+  }, []);
+  return (
+    <View style={{ flex: 1, flexDirection: 'vertical', alignItems: 'center', justifyContent: 'center' }}>
+        <Text style = {styles.passportPage}>Welcome {username}!</Text>
+        <Text style = {styles.passportPage}>Today's date is {date}</Text>
+        <Text style={[styles.passportPage, { color: '#39FF14' }]}>YOU HAVE A GREEN PASSPORT :)</Text>
+        <View style={(styles.navigationButtonStyle)}>
+          <Pressable style={styles.navigationButtonStyle} onPress={() => navigation.popToTop()}>
+            <Text>Go to home</Text>
+          </Pressable>
+        </View>
     </View>
   );
 }
@@ -158,12 +216,15 @@ function SettingsScreen({ navigation }) {
 const Stack = createNativeStackNavigator();
 
 function MyStack() {
+
+
   return (
     <Stack.Navigator>
       <Stack.Screen name="One Campus At Brandeis" component={HomeScreen} />
       <Stack.Screen name="About" component={AboutScreen} />
       <Stack.Screen name="Menu" component={MenuScreen} />
-      <Stack.Screen name="Settings" component={SettingsScreen} />
+      <Stack.Screen name="Covid Login" component={CovidLoginScreen} />
+      <Stack.Screen name="Covid Passport" component={CovidPassportScreen} />
     </Stack.Navigator>
   );
 }
@@ -210,7 +271,7 @@ const styles = StyleSheet.create({
   scrollView: {
     marginHorizontal: '5%',
   },
-  aboutPageText: {
+  mediumFont: {
     fontSize: 24
   },
   vibhuPic: {
@@ -218,6 +279,15 @@ const styles = StyleSheet.create({
     width: '95%',
     height: undefined,
     aspectRatio: 9 / 16
+  },
+  loginBox: {
+    fontSize: 24,
+    width: '70%',
+    marginTop: 5,
+    borderWidth: .5,
+  },
+  passportPage: {
+    fontSize: 32
   }
 })
 
